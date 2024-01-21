a75aaf2a7babccf57ec4c8c6ed5b3273c4166131
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-21 14:50:18 +0300

    .

--- src/components/OverlayLoader/OverlayLoader.tsx
+++ src/components/OverlayLoader/OverlayLoader.tsx
@@ -8,11 +8,14 @@ export const OverlayLoader = (props: Props) => {
 
 	return (
 		<>
-			{loadingState.requestCount > 0 && (
+			{loadingState.requestCount <= 0 && (
 				<div className="overlay">
 					<div className="overlay__inner">
 						<div className="overlay__content">
-							<span className="spinner"></span>
+							<img
+								className="spinner"
+								src="https://tobeto.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FTebeto-logo-yatay-beyaz.8c2d6927.png&amp;w=256&amp;q=75"
+							/>
 						</div>
 					</div>
 				</div>
